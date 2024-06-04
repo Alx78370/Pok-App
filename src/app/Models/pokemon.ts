6441d0c78bb2948cb2ad9b470eb182badcc85f5c
Alexis VACHET <alexis.vachet@outlook.fr>
2024-06-04 14:54:24 +0200

    first commit

--- src/app/Models/pokemon.ts
+++ src/app/Models/pokemon.ts
@@ -1,3 +1,320 @@
-export interface Pokemon {
+export interface Abilities {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
 
+export interface BaseExperience {
+  base_experience: number;
+}
+
+export interface Cries {
+  latest: string;
+  legacy: string;
+}
+
+export interface Forms {
+  name: string;
+  url: string;
+}
+
+export interface GameIndices {
+  game_index: number;
+  version: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Height {
+  height: number;
+}
+
+export interface HeldItems {
+  item: {
+    name: string;
+    url: string;
+  };
+  version_details: {
+    rarity: number;
+    version: {
+      name: string;
+      url: string;
+    };
+  };
+}
+
+export interface Id {
+  id: number;
+}
+
+export interface IsDefault {
+  is_default: boolean;
+}
+
+export interface LocationAreaEncounters {
+  location_area_encounters: string;
+}
+
+export interface Moves {
+  move: {
+    name: string;
+    url: string;
+  };
+  version_group_details: {
+    level_learned_at: number;
+    move_learn_method: {
+      name: string;
+      url: string;
+    };
+    version_group: {
+      name: string;
+      url: string;
+    };
+  };
+}
+
+export interface Name {
+  name: string;
+}
+
+export interface Order {
+  order: number;
+}
+
+export interface Species {
+  name: string;
+  url: string;
+}
+
+export interface Sprites {
+  back_default: string;
+  back_female: string;
+  back_shiny: string;
+  back_shiny_female: string;
+  front_default: string;
+  front_female: string;
+  front_shiny: string;
+  front_shiny_female: string;
+  other: {
+    dream_world: {
+      front_default: string;
+      front_female: string;
+    };
+    home: {
+      front_default: string;
+      front_female: string;
+      front_shiny: string;
+      front_shiny_female: string;
+    };
+    official_artwork: {
+      front_default: string;
+      front_shiny: string;
+    };
+    showdown: {
+      back_default: string;
+      back_female: string;
+      back_shiny: string;
+      back_shiny_female: string;
+      front_default: string;
+      front_female: string;
+      front_shiny: string;
+      front_shiny_female: string;
+    };
+  };
+  versions: {
+    generation_i: {
+      red_blue: {
+        back_default: string;
+        back_gray: string;
+        back_transparent: string;
+        front_default: string;
+        front_gray: string;
+        front_transparent: string;
+      };
+      yellow: {
+        back_default: string;
+        back_gray: string;
+        back_transparent: string;
+        front_default: string;
+        front_gray: string;
+        front_transparent: string;
+      };
+    };
+    generation_ii: {
+      crystal: {
+        back_default: string;
+        back_shiny: string;
+        back_shiny_transparent: string;
+        back_transparent: string;
+        front_default: string;
+        front_shiny: string;
+        front_shiny_transparent: string;
+        front_transparent: string;
+      };
+      gold: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+        front_transparent: string;
+      };
+      silver: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+        front_transparent: string;
+      };
+    };
+    generation_iii: {
+      emerald: {
+        front_default: string;
+        front_shiny: string;
+      };
+      firered_leafgreen: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+      };
+      ruby_sapphire: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+      };
+    };
+    generation_iv: {
+      diamond_pearl: {
+        back_default: string;
+        back_female: string;
+        back_shiny: string;
+        back_shiny_female: string;
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+      heartgold_soulsilver: {
+        back_default: string;
+        back_female: string;
+        back_shiny: string;
+        back_shiny_female: string;
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+      platinum: {
+        back_default: string;
+        back_female: string;
+        back_shiny: string;
+        back_shiny_female: string;
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+    };
+    generation_v: {
+      black_white: {
+        animated: {
+          back_default: string;
+          back_female: string;
+          back_shiny: string;
+          back_shiny_female: string;
+          front_default: string;
+          front_female: string;
+          front_shiny: string;
+          front_shiny_female: string;
+        };
+        back_default: string;
+        back_female: string;
+        back_shiny: string;
+        back_shiny_female: string;
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+    };
+    generation_vi: {
+      omegaruby_alphasapphire: {
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+      x_y: {
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+    };
+    generation_vii: {
+      icons: {
+        front_default: string;
+        front_female: string;
+      };
+      ultra_sun_ultra_moon: {
+        front_default: string;
+        front_female: string;
+        front_shiny: string;
+        front_shiny_female: string;
+      };
+    };
+    generation_viii: {
+      icons: {
+        front_default: string;
+        front_female: string;
+      };
+    };
+  };
+}
+
+export interface Stats {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Types {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Weight {
+  weight: number;
+}
+
+export interface Pokemon {
+  abilities: Abilities[];
+  base_experience: number;
+  cries: Cries;
+  forms: Forms[];
+  game_indices: GameIndices[];
+  height: number;
+  held_items: HeldItems[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: Moves[];
+  name: string;
+  order: number;
+  species: Species;
+  sprites: Sprites;
+  stats: Stats[];
+  types: Types[];
+  weight: number;
 }
--- src/app/poke-details/poke-details.component.ts
+++ src/app/poke-details/poke-details.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from '../Models/pokemon';
+import { EvolutionChain } from '../Models/evolution-chain';
+import { CommonModule } from '@angular/common';
+import { PokemonIdPipe } from '../pipes/pokemon-id.pipe';
+import { PokemonHeightPipe } from '../pipes/pokemon-height.pipe';
+import { tap, switchMap, catchError } from 'rxjs/operators';
+import { of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-poke-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PokemonIdPipe, PokemonHeightPipe],
   templateUrl: './poke-details.component.html',
   styleUrl: './poke-details.component.css'
 })
 export class PokeDetailsComponent {
+  pokemonDetails: Pokemon[] = [];
+  pokemonImages: any = [];
+  pokemonSpecies:any = [];
+  pokemonEvolutions: EvolutionChain[] = [];
+  pokeId: number = this.route.snapshot.params['id'];
+
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.pokemonService.getPokemonById(this.pokeId).pipe(
+      tap((data: any) => {
+        this.pokemonDetails.push(data);
+        console.log(this.pokemonDetails);
+      }),
+      switchMap(() => this.pokemonService.getSpeciesById(this.pokeId)),
+      tap((species: any) => {
+        this.pokemonSpecies.push(species);
+        console.log(this.pokemonSpecies);
+      }),
+      switchMap(() => {
+        const url = this.pokemonSpecies[0].evolution_chain.url;
+        const evolutionChainId = this.extractIdFromUrl(url);
+        return evolutionChainId ? this.pokemonService.getEvolutionsChainById(evolutionChainId) : of(null);
+      }),
+      tap((evolution: any) => {
+        if (evolution) {
+          this.pokemonEvolutions.push(evolution);
+          console.log(this.pokemonEvolutions);
+
+        }
+      }),
+    ).subscribe();
+  }
+
+
+  ngOnDestroy() {
+    this.pokemonDetails = [];
+    this.pokemonEvolutions = [];
+    this.pokemonSpecies = [];
+  }
+
+  getTypeColor(type: string) {
+    return this.pokemonService.getTypeColor(type);
+  }
+
+  extractIdFromUrl(url: string) : number {
+    const regex = /\/(\d+)\/?$/;
+    const match = url.match(regex);
+    return match ? parseInt(match[1], 10) : 0;
+  }
 
 }
